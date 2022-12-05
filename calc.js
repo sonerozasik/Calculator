669102f0730399ab7bdef5353946ad49daec0dca
Soner Özaşık <sonerozasik@gmail.com>
2022-12-05 10:50:46 +0300

    CSS done

--- calc.js
+++ calc.js
@@ -1,10 +1,10 @@
 var darkmode=0;
 function change(){
     if(darkmode==0){
-        document.getElementById("stylesheet").href="light.css";
+        document.getElementById("stylesheet").href="./light.css";
         darkmode=1;
     }else{
-        document.getElementById("stylesheet").href="dark.css";
+        document.getElementById("stylesheet").href="./dark.css";
         darkmode=0;
     }
     console.log(document.getElementById("stylesheet").href)
